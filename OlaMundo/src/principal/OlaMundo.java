71cd5e321eb269bd20a639003945c78f2e900abb
vanessa Avelar <vanessakavalar@gmail.com>
2023-08-02 17:54:19 -0300

    subindo os arquivos de java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,19 @@
+package principal;
+//classe principal
+public class OlaMundo {
+	//metodo principal, onde executa o programa
+	public static void main(String[] args) {
+	
+    //imprime uma linha e ao final salta uma nova linha
+    //print = imprimir; ln = new line;
+System.out.println("Hello word, Olá Mundo!");
+
+//comentárui de uma linha
+/*
+ * esse é
+ *um comentario
+ *de multiplas linhas
+ */
+	}
+
+}
