1462e6a629a324a2d3c4e2dd12f1d3b99ca6344e
vanessa Avelar <vanessakavalar@gmail.com>
2023-08-03 14:35:00 -0300

    projeto atividade 01 e 02 de 02/08/2023

---
+++ Atividade 01/src/projeto/Atividade1.java
@@ -0,0 +1,44 @@
+package projeto;
+
+public class Atividade1 {
+	
+	public static void main(String[] args) {
+		
+		int numero1 = 1;
+		int numero2 = 2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+		String nome = "VanessaAvelar";
+		
+		char sexo = 'F';
+		
+		System.out.println("vanessaavelar");
+		System.out.println(sexo);
+		
+		boolean verdadeiro = true;
+		
+		boolean falso = false;
+		
+		System.out.println(verdadeiro);
+		System.out.println(falso);
+		
+		System.out.println("*********************");
+		
+		System.out.println("Atividade02");
+		
+		String nome1 = "Vanessa ";
+		
+		String nome2 = "Avelar";
+		
+		System.out.println(nome1 + nome2);
+		
+		
+		
+		
+		
+		
+	}
+
+}
